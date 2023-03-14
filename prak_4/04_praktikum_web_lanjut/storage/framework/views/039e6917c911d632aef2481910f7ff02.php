652b971b053ff7627f4bdcf735d54203d0c1ac53
ilhamydn17 <ilhamyudantyo@gmail.com>
2023-03-15 00:37:20 +0700

    finishin

---
+++ prak_4/04_praktikum_web_lanjut/storage/framework/views/039e6917c911d632aef2481910f7ff02.php
@@ -0,0 +1,45 @@
+<?php $__env->startSection('heading','Welcome User!'); ?>
+
+<?php $__env->startSection('content'); ?>
+
+<div class="row">
+
+    <div class="col-xl-3 col-md-6 mb-4">
+       <div class="card border-left-primary shadow p-3">
+           
+           <h5 class="card-title mt-1">Marbel Edu</h5>
+           <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+       </div>
+    </div>
+
+    <div class="col-xl-3 col-md-6 mb-4">
+       <div class="card border-left-warning shadow p-3">
+           
+           <h5 class="card-title mt-1">Marbel and Friends</h5>
+           <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+       </div>
+    </div>
+
+    <div class="col-xl-3 col-md-6 mb-4">
+       <div class="card border-left-success shadow p-3">
+           
+           <h5 class="card-title mt-1">Riri Story</h5>
+           <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+       </div>
+    </div>
+
+    <div class="col-xl-3 col-md-6 mb-4">
+       <div class="card border-left-danger shadow p-3">
+           
+           <h5 class="card-title mt-1">Kolak Kids Song</h5>
+           <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+       </div>
+    </div>
+
+
+   <!-- Earnings (Monthly) Card Example -->
+   
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\1. KULIAHH\SEMESTER-4\Web Lanjut\1.0 Folder Projects_2\prak_larv_projects\prak_4\04_praktikum_web_lanjut\resources\views/home-2.blade.php ENDPATH**/ ?>
